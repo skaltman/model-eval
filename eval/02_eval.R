f77db7e844b73d569fdf08429ab0573103f6bfcc
skaltman <sara.altman@posit.co>
2025-11-25 10:12:13 -0800

    add gpt 5.1

--- eval/02_eval.R
+++ eval/02_eval.R
@@ -8,12 +8,20 @@ SCORER_MODEL <- "claude-3-7-sonnet-latest"
 
 # -------------------------------------------------------------------------------------------------------
 vitals::vitals_log_dir_set(here::here("logs/02_eval"))
-source(here::here("scripts/helper.R"))
+source(here::here("eval/helper.R"))
 
 scorer_chat <- chat_anthropic(model = SCORER_MODEL)
 
+# Gemini 3
 model_eval(
   model = "gemini-3-pro-preview",
   filename = "gemini_3",
   chat_fun = chat_google_gemini
+)
+
+# GPT-5.1
+model_eval(
+  model = "gpt-5.1",
+  filename = "gpt_5_1",
+  chat_fun = chat_openai
 )
